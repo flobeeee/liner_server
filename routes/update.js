dad62a916704e7a76397814ad7cf22bae76a7334
EunJeong Lee <sssver28@gmail.com>
2021-05-20 11:48:51 +0900

    add 6.update API

--- routes/update.js
+++ routes/update.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const updateController = require('../ controllers/update');
+const themeController = require('../ controllers/theme');
 const router = express.Router();
 
 router.patch('/highlight', updateController.update);
+router.put('/theme', themeController.update);
 
 module.exports = router;
